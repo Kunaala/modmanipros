bc8a833fbdd94c85b23e5b4d42621c18d2392eb2
kunaala <kunaala>
2020-07-03 16:54:39 +0530

    updated extras exchrono

--- src/modmanipros/extras/exchrono.cpp
+++ src/modmanipros/extras/exchrono.cpp
@@ -1,21 +1,20 @@
 #include<iostream>
 #define BOOST_CHRONO_VERSION 2
 #include<boost/chrono.hpp>
-#include<typeinfo>
 #include <sstream> 
 #include<string>
+//#include<typeinfo>
 int main()
 {	namespace chrono=boost::chrono;
-	//std::cout << chrono::system_clock::now() <<std::endl;//system time
-	std::cout << chrono::steady_clock::now() <<std::endl;//time since last boot
-  	std::cout << chrono::time_fmt(boost::chrono::timezone::utc, "%D") <<
-    chrono::system_clock::now() << '\n';
-	std::cout << chrono::time_fmt(boost::chrono::timezone::utc, "%H:%M:%S") <<
-    chrono::system_clock::now() << '\n';
-	std::stringstream temp;
-	temp<<chrono::time_fmt(boost::chrono::timezone::utc, "%H:%M:%S")<<chrono::system_clock::now();
-	std::string temp1;
-	temp>>temp1;
-	std::cout<<temp1;
+	//std::cout << chrono::steady_clock::now() <<std::endl;		//time since last boot
+  	//std::cout << chrono::time_fmt(boost::chrono::timezone::utc, "%D") << chrono::system_clock::now() << '\n';
+	//std::cout << chrono::time_fmt(boost::chrono::timezone::utc, "%H:%M:%S") << chrono::system_clock::now() << '\n';
+	std::stringstream temp1,temp2;
+	std::string datestr,timestr;
+	temp1<<chrono::time_fmt(boost::chrono::timezone::utc, "%H:%M:%S")<<chrono::system_clock::now();
+	temp2<<chrono::time_fmt(boost::chrono::timezone::utc, "%D") << chrono::system_clock::now();
+	temp1>>datestr;
+	temp2>>timestr;
+	std::cout<<datestr<<":"<<timestr;
 	return 0;
 }
